89dd9ccba57a15436881614678950cb02f16ddd9
DanieleFlo <daniele.florio00@gmail.com>
2024-11-08 16:43:38 +0100

    Update gestionale.py

--- 8 Novembre/gestionale.py
+++ 8 Novembre/gestionale.py
@@ -90,8 +90,25 @@ class Users():
                 return True
         return False
     
+    def get_users_ord_score(self):
+        temp = {}
+        for key, user in self.__users.items():
+            temp[key] ={'score': user.get_score()}
+            self.__users[key].get_score()
+        temp = sorted(temp, key=lambda x: x['score'])
+        return self.__deconstructor()
+            
+    def __deconstructor(dizionario):
+        stringa = ""
+        for elemento in dizionario.values():
+            parziale = "Giocatore: " + elemento.get_username() + " punteggio: " + elemento.get_score()
+            stringa += parziale
+        return stringa
+
+    
     
 users = Users()
-print(users.register())
-print(users.login())
-print(users.register())
+# print(users.register())
+# print(users.login())
+# print(users.register())
+# print(users.get_users_ord_score())
